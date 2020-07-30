f7ee966cb7687672573d7bcf0396d751f9c08d31
Quinn <quinnvoker@gmail.com>
2020-07-30 20:27:43 +0000

    completed the assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,16 @@
 const request = require('request');
 
 const apiAddress = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breed = process.argv[2].slice(0,4);
+const breed = process.argv[2];
 
-request(apiAddress + breed, (error, response, body) => {
+request(apiAddress + breed.slice(0,4), (error, response, body) => {
   if (error) {
     throw error;
   }
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  if (data.length < 1) {
+    console.log(`Breed '${breed}' not found in database!`);
+    return;
+  }
+  console.log(data[0].description);
 });
