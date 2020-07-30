2244cda857b0986356a620e7a6a9ecf94d1eb464
Quinn <quinnvoker@gmail.com>
2020-07-30 20:38:27 +0000

    refactoring complete

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
 const request = require('request');
 
 const apiAddress = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breed = process.argv[2];
 
-request(apiAddress + breed.slice(0,4), (error, response, body) => {
-  if (error) {
-    throw error;
-  }
-  const data = JSON.parse(body);
-  if (data.length < 1) {
-    console.log(`Breed '${breed}' not found in database!`);
-    return;
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(apiAddress + breedName.slice(0,4), (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length < 1) {
+      callback(`Breed '${breedName}' not found in database!`);
+      return;
+    }
+    callback(error, data[0].description);
+  });
+};
+
+module.exports = fetchBreedDescription;
